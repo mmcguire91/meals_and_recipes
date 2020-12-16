137424814743ccce9d9ca50e3db300a3cf94954a
mmcguire91 <mmcguire91@gmail.com>
2020-12-16 16:30:48 -0500

    added categories_view as home for view on app open, wrapped gridview with scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'views/categories_view.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,29 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CategoriesView(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Select a Category'),
-      ),
-      body: Center(
-        child: Text(
-              'Text'
-            ),
-        ),
-      );
   }
 }
--- lib/views/categories_view.dart
+++ lib/views/categories_view.dart
@@ -6,22 +6,28 @@ import 'package:meals_and_recipes/widgets/category.dart';
 class CategoriesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
       ),
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => Category(
-              title: catData.title,
-              color: catData.color,
-            ),
-          )
-          .toList(),
-      //take the existing data and map to the Category UI component
+      body: GridView(
+        //GridView is meant as a body element that contains minor UI components and data
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => Category(
+                title: catData.title,
+                color: catData.color,
+              ),
+            )
+            .toList(),
+        //take the existing data and map to the Category UI component
+      ),
     );
   }
 }
