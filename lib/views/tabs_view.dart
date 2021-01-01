1479cc409089f879e108613fd3948137a0fe1114
mmcguire91 <mmcguire91@gmail.com>
2021-01-01 13:57:31 -0500

    in progress of transferring tabs from menu bar to bottomNavigationBar

--- lib/views/tabs_view.dart
+++ lib/views/tabs_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_and_recipes/views/favorites.dart';
 
+import './favorites.dart';
 import './categories_view.dart';
 
 //TabsView is the tab controller on the appbar
@@ -13,36 +13,27 @@ class TabsView extends StatefulWidget {
 class _TabsViewState extends State<TabsView> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      // initialIndex: 0,
-      // initial index defines the tab that you want the user to see on initial load
-      // the default logic is to display the view that is mapped to the 1st tab which is what we want so it isn't required that we specify that initial index = 0
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesView(),
-            Favorites(),
-          ],
-        ),
-        //appropriately map the tabs to display the appropriate view
-        //the logic must meet the order in which each is defined i.e. 1st tab = 1st listed widget in body
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
       ),
+      body: null,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: null,
+          backgroundColor: Theme.of(context).canvasColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+            ),
+          ]),
     );
   }
 }
+
+//appropriately map the tabs to display the appropriate view
+//the logic must meet the order in which each is defined i.e. 1st tab = 1st listed widget in body
